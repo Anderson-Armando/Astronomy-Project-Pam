262dedb1e82c26bed7074435cc2da9c7d5ce62bb
Anderson-Armando <arevalosanderson9@gmail.com>
2024-09-09 22:14:58 -0300

    Projeto quase finalizado com erros

---
+++ PAM-Astronomy/Views/AstronomicalBodyView.xaml.cs
@@ -0,0 +1,47 @@
+namespace PAM_Astronomy.Views;
+
+[QueryProperty(nameof(AstroName), "astroName")]
+
+public partial class AstronomicalBodyView : ContentPage
+{
+	public AstronomicalBodyView()
+	{
+		InitializeComponent();
+	}
+
+	string astroName;
+	public string AstroName
+	{
+		get => astroName;
+		set
+		{
+			astroName = value;
+			UpdateAstroBodyUI(astroName);
+		}
+	}
+
+	void UpdateAstroBodyUI(string astroName)
+	{
+		AstronomicalBodyView body = FindAstroData(astroName);
+
+        Title = body.Title;
+
+        lblIcon.Text = body.EmojiIcon;
+		lblName.Text = body.Name;
+		lblMass.Text = body.Mass;
+		lblCircumference.Text = body.Circumference;
+		lblAge.Text = body.Age;
+	}
+
+	AstronomicalBodyView FindAstroData(string astronomicalBodyName)
+	{
+		return astronomicalBodyName switch
+		{
+			"comet" => SolarSystemData.HalleysComet,
+			"earth" => SolarSystemData.Earth,
+			"moon" => SolarSystemData.Moon,
+			"sun" => SolarSystemData.Sun, _=> throw new ArgumentException()
+		};
+	}
+
+}
